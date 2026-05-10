0e8d8ae1429d7a62f527332ee09bac38c552b5b7
Pedro Pérez Rodríguez <perezrodriguezpedro.w@gmail.com>
2026-05-10 20:31:21 +0200

    feat: implementar ingesta multivariable de métricas hacia HDFS

--- exportar_a_hive.py
+++ exportar_a_hive.py
@@ -4,28 +4,34 @@ import subprocess
 
 PROMETHEUS_URL = "http://localhost:9090/api/v1/query"
 
+METRICAS = {
+    'node_load1': 'cpu_load',
+    'node_memory_MemAvailable_bytes': 'recurso_disponible'
+}
+
 def exportar_directo():
-    print("Iniciando ingesta directa en Hive (Ctrl+C para detener)...")
+    print("Iniciando ingesta directa en Hive (HDFS) - (Ctrl+C para detener)...")
     while True:
         try:
-            response = requests.get(PROMETHEUS_URL, params={'query': 'node_load1'})
-            results = response.json()['data']['result']
-            
-            for res in results:
-                ts = time.strftime('%Y-%m-%d %H:%M:%S')
-                nodo = res['metric'].get('instance', 'nodo-desconocido')
-                valor = res['value'][1]
+            for query_prom, nombre_metrica in METRICAS.items():
+                response = requests.get(PROMETHEUS_URL, params={'query': query_prom})
+                results = response.json()['data']['result']
                 
-                sql = f"INSERT INTO TABLE metricas_hadoop VALUES ('{ts}', '{nodo}', 'cpu_load', {valor});"
-                
-                print(f"[{ts}] Insertando en clúster: {nodo} -> {valor}")
-                
-                subprocess.run([
-                    "docker", "exec", "hive2", "beeline", 
-                    "-u", "jdbc:hive2://localhost:10000", 
-                    "-n", "root",
-                    "-e", sql
-                ], capture_output=True)
+                for res in results:
+                    ts = time.strftime('%Y-%m-%d %H:%M:%S')
+                    nodo = res['metric'].get('instance', 'nodo-desconocido')
+                    valor = res['value'][1]
+                    
+                    sql = f"INSERT INTO TABLE metricas_hadoop VALUES ('{ts}', '{nodo}', '{nombre_metrica}', {valor});"
+                    
+                    print(f"[{ts}] Insertando {nombre_metrica} en clúster: {nodo} -> {valor}")
+                    
+                    subprocess.run([
+                        "docker", "exec", "hive2", "beeline", 
+                        "-u", "jdbc:hive2://localhost:10000", 
+                        "-n", "root",
+                        "-e", sql
+                    ], capture_output=True)
 
             time.sleep(10)
             
